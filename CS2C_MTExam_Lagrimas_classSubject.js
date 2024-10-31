23260669ef4ab2809ecea62b1ea299b03209c337
KarryleJS-practice <karrylelagrimas6@gmail.com>
2024-10-31 10:27:07 +0800

    CS2C_MTExam_Lagrimas_classSubject.js

---
+++ CS2C_MTExam_Lagrimas_classSubject.js
@@ -0,0 +1,9 @@
+let studentName = prompt("Karryle B. Lagrimas:");
+let subjectTitle = prompt("Data Structures and Algorithms:");
+let classSchedule = prompt("10:30-11-30, 8:00-9:30, 4 days:");
+let classroom = prompt("Computer Laboratory B:");
+let classInstructor = prompt("Clifford Owen Pascua:");
+
+let outputString = `${"Karryle B. Lagrimas"} is currently enrolled in ${"Data Structures and Algorithms"} with a class schedule of ${"10:30-11-30, 8:00-9:30, 4 days"} at room ${"Computer Laboratory"}. The instructor for the subject is ${"Clifford Owen Pascua"}`;
+
+console.log(outputString);
